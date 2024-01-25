a64c4edc90b06eefb53a0aff07196483a41a9df8
Marine Le Cleach <lecleach.m@gmail.com>
2024-01-25 09:25:41 +0100

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import CompanyProfilePage from './pages/CompanyProfilePage.jsx'
+import CompanyActivityDetailsPage from './pages/CompanyActivityDetailsPage'
+import PersonProfilePage from './pages/PersonProfilePage'
+import PersonMyActivitiesPage from './pages/PersonMyActivitiesPage'
+import Navbar from './components/Navbar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   <Routes>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/company/:companyId' element={<CompanyProfilePage />} />
+        <Route path='/company/:activityId' element={<CompanyActivityDetailsPage />} />
+        <Route path='/' element={<CompanyCreateActivityPage />} />
+        <Route path='/personProfile' element={<PersonProfilePage />} />
+        <Route path='/' element={<PersonMyActivitiesPage />} />
+        <Route path='*' element={<h1>404 Page not found</h1>} />
+        </Routes>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+ 
+
+  return (
+    <nav>
+  
+    </nav>
+  )
+}
+
+export default Navbar
