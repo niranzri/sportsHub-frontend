aa88f5677aa966505966542747888861635f2421
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-01-25 18:06:56 +0100

    started Login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,40 @@
 import { useState } from "react";
-import { Link } from "react-router-dom"
+import { useNavigate, Link } from 'react-router-dom'
+
 const LoginPage = () => {
 
 const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
 
+const navigate = useNavigate()
+
+const handleSubmit = async (event) => {
+    event.preventDefault();
+    const credentials = { email, password };
+
+    try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(credentials),
+        });
+
+        if (response.status === 200) {
+            // If user was logged in successully
+            const parsed = await response.json()
+            console.log(parsed)
+            saveToken(parsed.authToken)
+            // navigate('/') // change navigation once we have the routes figured out
+          }
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 return ( 
     <>
-    <form  style={{ display: 'flex', flexDirection: 'column' }}>
+    <form onSubmit={handleSubmit}  style={{ display: 'flex', flexDirection: 'column' }}>
         <label>Email:
             <input value={email} onChange={event => setEmail (event.target.value)} required/></label>
         <label>Password:
