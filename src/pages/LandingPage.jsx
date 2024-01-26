2a5475aa07529b3891a9a4f1ce6c9dda8a466c28
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-01-26 17:46:25 +0100

    Landing Page explore button

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import classes from '../styles/landing.module.css'
 
+
 const LandingPage = () => {
+
+const navigate = useNavigate()
+const handleExploreClick = () => {
+    navigate('/allActivities'); 
+  }; 
+
     return (  
         <>
             <div className={classes.hero}>
                 <h1>Unleash Your Fitness Potential</h1>
                 <p>Access hundreds of sports facilities and fitness classes all in one app</p>
+                 <div className={classes.buttonCtn}>
+                     <button type='button' onClick={handleExploreClick}>Explore</button>
             </div>
-            <div className={classes.content}>
+           { /* <div className={classes.content}>
                 <p>More information about our site</p>
+          </div> */}
             </div>
         </>
     );
