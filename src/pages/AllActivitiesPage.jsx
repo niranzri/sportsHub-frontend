e950b2e9fee030a59bdc1195585ac67d53b29cad
Marine Le Cleach <lecleach.m@gmail.com>
2024-01-26 15:16:57 +0100

    add style

--- src/pages/AllActivitiesPage.jsx
+++ src/pages/AllActivitiesPage.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Navbar from '../components/Navbar.jsx';
+import classeActivities from '../styles/allActivities.module.css'
 
 const AllActivitiesPage = () => {
- /*   const [activities, setActivities] = useState([])
+    const [activities, setActivities] = useState([])
     const fetchActivities = async () => {
         try {
           const response = await fetch(`${import.meta.env.VITE_API_URL}/api/activities`)
@@ -20,7 +22,29 @@ const AllActivitiesPage = () => {
       useEffect(() => {
         fetchActivities()
       }, [])
-    */
+      return (
+        <div>
+            <Navbar/>  
+            <div className={classeActivities.container}>
+          
+    
+            {activities.map(activity => (
+                <div className={classeActivities.activity} key={activity._id}>
+            <div className={classeActivities.item} >
+              
+                <Link to={`/activityDetails/${activity._id}`}>
+                  <p>{activity.type}</p>
+                  {<img src={activity.image}  />}
+                </Link>
+              
+             </div>
+        </div>
+            ))}
+          
+          </div>
+        </div>
+      )
+    
 }
 
 
