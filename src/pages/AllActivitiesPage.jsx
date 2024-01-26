a17d7012c6cd2df90c8bf71f768a2795ad1ef10b
Marine Le Cleach <lecleach.m@gmail.com>
2024-01-26 14:46:55 +0100

    export page

---
+++ src/pages/AllActivitiesPage.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+const AllActivitiesPage = () => {
+ /*   const [activities, setActivities] = useState([])
+    const fetchActivities = async () => {
+        try {
+          const response = await fetch(`${import.meta.env.VITE_API_URL}/api/activities`)
+          if (response.ok) {
+            const activitiesData = await response.json()
+            console.log(activitiesData)
+            setActivities(activitiesData)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+
+
+      useEffect(() => {
+        fetchActivities()
+      }, [])
+    */
+}
+
+
+
+
+export default AllActivitiesPage
