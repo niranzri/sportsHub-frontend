6e000ed071b7b479b0b4a6810d9b1027c036260b
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-01-26 17:25:38 +0100

    ActivitiyDetails page-working

--- src/pages/ActivityDetailsPage.jsx
+++ src/pages/ActivityDetailsPage.jsx
@@ -1,8 +1,10 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import classes from '../styles/activitiesDetails.module.css'
 
 
 const ActivityDetailsPage = () => {
+    
   const { activityId } = useParams('');
   const [activity, setActivity] = useState({});
   const navigate = useNavigate()
@@ -10,7 +12,7 @@ const ActivityDetailsPage = () => {
  
     const fetchActivity = async () => {
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/activityDetails/${activityId}`)
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/activities/${activityId}`)
         if (response.ok) {
           const activityData = await response.json()
           setActivity(activityData)
@@ -32,16 +34,43 @@ const ActivityDetailsPage = () => {
 
   return (
     <>
-      <h1>Activity Details</h1>
-      <p>{activity.type}</p>
-      <p>{activity.scheduale}</p>
-      <button type='button' onClick={handleBackClick}>
-        Back
-      </button>
-          
-      </>
-
+      <div className={classes.mainCtn}>
+        <div className={classes.imageContainer}>
+          <img src={activity.image} alt="Activity Image" />
+        </div>
+        <div className={classes.textCtn}>
+          <p>{activity.type}</p>
+          <p>{activity.schedule?.join(', ')}</p>
+          <div className={classes.buttonCtn}>
+            <button type='button' onClick={handleBackClick}>
+              Back
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default ActivityDetailsPage;
+ export default ActivityDetailsPage;
+
+/* return (
+    <div>
+      <Navbar />
+      <div className={classesArtDetailsStyle.mainCtn}>
+       <div className={classesArtDetailsStyle.imageContainer}>
+         <img
+           src={art.img}
+            alt={art.alt_text}
+         />
+       </div>
+      <div className={classesArtDetailsStyle.textCtn}>
+        <p>
+          <span className={classesArtDetailsStyle.label}>Title:</span>{' '}
+          {isEditing ? (
+            <input
+              type="text"
+              value={art.title}
+              onChange={(e) => onChangeHandler('title', e.target.value)}
+              className={classesArtDetailsStyle.editInput}
+            /> */
