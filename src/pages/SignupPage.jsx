472c7c18e45d1161e01f826e82944d5fc3c7df4c
Noelia <noeliairanzoribera@gmail.com>
2024-01-29 12:37:07 +0100

    fixed hashed password and added notifications

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom'
 import classes from '../styles/form.module.css';
 import Select from 'react-select';
-// import { AuthContext } from "../contexts/AuthContext"
 
 const SignupPage = () => {
 
@@ -10,11 +9,14 @@ const [name, setName] = useState('');
 const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
 const [companies, setCompanies] = useState([]);
-const [company, setCompany] = useState([]);
+const [company, setCompany] = useState(null);
+
+const [isPasswordValid, setIsPasswordValid] = useState(true);
+const [isEmailValid, setIsEmailValid] = useState(true);
 
 const navigate = useNavigate()
 
-const fetchCompanies = async () => {
+  const fetchCompanies = async () => {
     try {
       const response = await fetch(`${import.meta.env.VITE_API_URL}/api/companies`)
       if (response.ok) {
@@ -37,9 +39,37 @@ const fetchCompanies = async () => {
     fetchCompanies()
   }, [])
 
-const handleSubmit = async (event) => {
+  const handleEmailChange = (value) => {
+    setEmail(value);
+    // Checks if the password is empty and hides it from display
+    if (value.trim() === '') {
+      setIsEmailValid(true);
+    } else {
+    // Checks the password format and update the state
+      setIsEmailValid(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(value));
+    }
+  }
+
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+    // Checks if the password is empty and hides it from display
+    if (value.trim() === '') {
+      setIsPasswordValid(true);
+    } else {
+    // Checks the password format and update the state
+      setIsPasswordValid(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/.test(value));
+    }
+  }
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log("Selected Company:", company.value); 
+
+    if (!isPasswordValid) {
+      console.log("Password does not meet the format requirements");
+      return;
+    }
+
+    //console.log("Selected Company:", company.value); 
     const credentials = { name, email, password, company: company.value };
     console.log(credentials)
 
@@ -58,30 +88,44 @@ const handleSubmit = async (event) => {
     } catch (error) {
         console.log({error, message: "Error while posting user"});
     }
-};
+  };
 
- 
-return (
+  return (
     <div className={classes.pageCtn}> 
-    <form onSubmit={handleSubmit} className={classes.form}>
+      <form onSubmit={handleSubmit} className={classes.form}>
         <label> <span> Name: </span>
-            <input 
+          <input 
                 value={name} 
-                onChange={event => setName (event.target.value)} 
+                onChange={event => setName(event.target.value)} 
                 required/>
         </label>
         <label> <span> E-mail: </span>
             <input 
                 value={email} 
-                onChange={event => setEmail (event.target.value)} 
+                onChange={event => handleEmailChange(event.target.value)} 
                 required/>
         </label>
+        <div className={classes.notificationCtn}>
+          {!isEmailValid && (
+            <div className={classes.notification}>
+              * Please provide a valid e-mail address.
+            </div>
+          )}
+        </div>
         <label> <span> Password: </span>
-                <input 
+            <input 
+                type="password"
                 value={password} 
-                onChange={event => setPassword (event.target.value)} 
+                onChange={event => handlePasswordChange(event.target.value)} 
                 required/>
         </label>
+        <div className={classes.notificationCtn}>
+          {!isPasswordValid && (
+            <div className={classes.notification}>
+              * Password must have at least 6 characters and contain at least one number, one lowercase, and one uppercase letter.
+            </div>
+          )}
+        </div>
         <label> <span> Company: </span> 
                 <div className={classes.companyCtn}>
                 <Select 
@@ -123,7 +167,7 @@ return (
                 </div>
         </label>
         <button type='submit' className={classes.accessButton}> Sign Up </button>
-    </form>
+      </form>
     </div>
  );
 }
