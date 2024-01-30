73f365592fb063ebf4105f58fc38a141bfce2162
Noelia <noeliairanzoribera@gmail.com>
2024-01-30 14:49:21 +0100

    profile component works

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -4,40 +4,42 @@ import { AuthContext } from '../contexts/AuthContext'
 import classes from '../styles/profile.module.css';
 
 const ProfileSection = () => {
-  const { user } = useContext(AuthContext)
-  const [userInfo, setUserInfo] = useState();
-  const [companyInfo, setCompanyInfo] = useState(null);
+  //const { user } = useContext(AuthContext)
+  const { userId } = useParams();
+  const [userInfo, setUserInfo] = useState([]);
+  const [company, setCompany] = useState([]);
 
-  console.log(user)
+  //console.log(user)
 
-  if (!user) {
-    return null; 
-  }
+  //if (!user) {
+  //  return null; 
+  //}
   
-  const fetchUserInfo = async () => {
-    try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/profile/${user._id}`);
-      if (response.ok) {
-        const userData = await response.json();
-        setUserInfo(userData);
-      } else {
-        console.log(`Couldn't fetch the user data`);
+  useEffect (() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/profile/${userId}`);
+        if (response.ok) {
+          const userData = await response.json();
+          setUserInfo(userData);
+        } else {
+          console.log(`Couldn't fetch the user data`);
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    };
 
-  
-  useEffect(() => {
-    fetchUserInfo()
-  }, [])
-  
+
+  fetchUserInfo()}, [userId]
+  )
+
+  console.log(userInfo)
 
     return (
         <>
             <h1> {userInfo.name} </h1>
-            <h2> {userInfo.company}</h2>
+            <h3> {userInfo.company.name} </h3>
         </>
     );
 }
