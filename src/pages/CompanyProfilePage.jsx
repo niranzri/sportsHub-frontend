6179f467b1dfb7c045215d1f28df1e3f73274e09
Marine Le Cleach <lecleach.m@gmail.com>
2024-01-31 12:05:20 +0100

    add create activity

--- src/pages/CompanyProfilePage.jsx
+++ src/pages/CompanyProfilePage.jsx
@@ -3,14 +3,11 @@ import { Link } from 'react-router-dom'
 import classes from '../styles/profile.module.css';
 import ProfileSection from '../components/ProfileSection.jsx'
 import { AuthContext } from '../contexts/AuthContext'
-//import classeProfile from '../styles/profile.module.css'
 
 const CompanyProfilePage = () => {
 
     let company = '';
     const [activities, setActivities] = useState([])
-
-    //const { userId } = useParams();
     
 
     const { user } = useContext(AuthContext)
@@ -18,29 +15,7 @@ const CompanyProfilePage = () => {
     company = companyId;
     console.log(user)
 
-/*
-    useEffect (() => {
-        const fetchUserInfo = async () => {
-          try {
-            const response = await fetch(`${import.meta.env.VITE_API_URL}/profile/${user._id}`);
-            if (response.ok) {
-             
-              const userData = await response.json();
-              setUserInfo(userData);
-            
-            } else {
-              console.log(`Couldn't fetch the user data`);
-            }
-          } catch (error) {
-            console.log(error);
-          }
-        };
-        if(user){
-      fetchUserInfo()
-        }
-      }, [user]
-      )*/
-    
+
      useEffect(() => {
       
            
@@ -53,11 +28,7 @@ const CompanyProfilePage = () => {
                console.log(response)
                if (response.ok) {
                  const activitiesData = await response.json()
-      
-                 console.log(activitiesData)
                  setActivities(activitiesData)
-                 console.log(activitiesData[0].image)
-                 console.log(activitiesData[0].type)
                }
              } catch (error) {
                console.log(error)
@@ -82,38 +53,22 @@ const CompanyProfilePage = () => {
             <div className={classes.activitiesCtn}>
 
                 <h1>All the activities of your company</h1>
+                <Link to={`/companies/${user?.company}/createActivity`}>Create an activity</Link>
                 {activities.map(activity => (
-    <div className={classes.activity} key={activity._id}>
-<div className={classes.item} >
+                 <div className={classes.activity} key={activity._id}>
+                    <div className={classes.item} >
 
-  {<Link to={`/companies/${companyId}/${activity._id}`}>
-    <p className={classes.text}>{activity.type}</p>
-      {<img src={activity.image}  />}
-    </Link>      
-  }
-   
-  </div>
-        </div> ))}
-
-
-              {/*  activities.map(activity => (
-                <div className={classeActivities.activity} key={activity._id}>
-            
-            
-                <div>
-                <p>{activity.type}</p>
-                  {<img src={activity.image} alt="Activity Image" />}
-                  </div>
-            
-        </div>
-              )) */}
+                          {<Link to={`/companies/${companyId}/${activity._id}`}>
+                                 <p className={classes.text}>{activity.type}</p>
+                                 {<img src={activity.image}  />}
+                          </Link> }     
                 
 
+                     </div>
+                 </div> ))}
 
             </div>
         </div>
-
-
 
     );
               }
